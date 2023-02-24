b6a8dbb674d936afe5fbb1d1733232dec77f7936
Caroline <117231122+carolinefonseca0001@users.noreply.github.com>
2023-02-24 16:09:50 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function evenOrOdd(number) {
+    if (number%2 == 0) {
+      console.log (`${number} is Even`)
+    }
+    else {console.log (`${number} is Odd`)}
+  }
+  
+  evenOrOdd(2)
+  evenOrOdd(7)
+  evenOrOdd(-42)
+  evenOrOdd(-7)
+  evenOrOdd(0)
